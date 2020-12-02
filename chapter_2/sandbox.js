ccf7766703b95486eb3eb2a07b796dfac59c2fa3
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-02 15:54:38 +0100

    Finishing 2.11 Numbers and 2.12 Template strings.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -27,13 +27,43 @@
 // let result = fullName.toLowerCase();
 // let indexOfAt = email.indexOf('@')
 // console.log(email.indexOf('a'))
+// -----------------------------------------------------
+// // String methods 2 // common string methods
+// let email = "johan@hotmale.com";
+// console.log(email.lastIndexOf("a"));
+// let firstName = email.slice(0, 5);
+// console.log(firstName);
+// let result = email.substr(6, 7);
+// console.log(result);
+// let lastName = email.replace("a", "P");
+// console.log(lastName);
+// ------------------------------------------------------
 
-// String methods 2 // common string methods
-let email = "johan@hotmale.com";
-console.log(email.lastIndexOf("a"));
-let firstName = email.slice(0, 5);
-console.log(firstName);
-let result = email.substr(6, 7);
-console.log(result);
-let lastName = email.replace("a", "P");
-console.log(lastName);
+// Numbers
+
+let radius = 20;
+const pi = 3.14;
+let area = radius * radius * pi;
+let circumference = 2 * radius * pi;
+
+console.log(`Här kommer en hälsning från area: ${area} cm`);
+console.log(
+  `Här kommer en annan hälsning från omkrets: ${Math.round(circumference)} cm`
+);
+
+let likes = 100;
+
+// likes = ++ likes;
+likes++;
+likes--
+--likes
+likes = -- likes
+likes =- likes
+likes -= likes
+likes +=10
+likes /= 2
+likes *=20
+likes = likes + 1
+console.log(likes);
+
+console.log( 'hello'.toUpperCase())
