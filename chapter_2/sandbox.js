933d9f5395be47b45fc34b5690bd4053c9536972
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-02 10:56:27 +0100

    Finishing until 2.9 string methods.

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,29 @@
+// let name = "Johan";
+// let birthYear = 1994;
+// let age = 2020 - 1994;
+// console.log(`Hi Johan. Your birthYear is ${birthYear}. That means you are ${age} years old.`);
+
+// strings
+console.log("Hello there, World!");
+
+let email = "johan@hotmale.com";
+console.log(email);
+
+// string concatenation
+
+let firstName = "Johan";
+let lastName = "Berglund";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Getting characters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length - 1);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+let indexOfAt = email.indexOf('@')
+console.log(email.indexOf('a'))
