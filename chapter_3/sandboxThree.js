c60d3e8b7767a3dad09d1ef730f3f5e7a2baf3e4
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-03 15:49:34 +0100

    Adding 3.19, 3.20 and 3.21 For loop, while loop and do while loop.

--- chapter_3/sandboxThree.js
+++ chapter_3/sandboxThree.js
@@ -1,13 +1,42 @@
+// // 3.18 & 19 Control flow & While loops
+
 // for (let i = 0; i < 5; i++) {
 //   console.log("in loop :", i);
 // }
 
 // console.log('The loop is now finished')
 
-const names = ["shaun", "mario", "Johnny"];
+// const names = ["shaun", "mario", "Johnny"];
 
-for (let i = 0; i < names.length; i++) {
-  // console.log(names[i])
-  let html = `<div>${names[i]}</div>`;
-  console.log(html)
-}
+// for (let i = 0; i < names.length; i++) {
+//   // console.log(names[i])
+//   let html = `<div>${names[i]}</div>`;
+//   console.log(html)
+// }
+
+// // 3.20 While loops
+
+// let i = 0;
+// while (i < 5) {
+//   i++;
+//   console.log("in loop :", i);
+// }
+
+// const names = ["shaun", "mario", "Johnny"];
+
+//let i=0
+
+// while(i < names.length) {
+//   console.log(names[i])
+//   i++
+// }
+
+// // 3.21 Do While loops
+
+
+let i = 10;
+
+ do {
+  i++;
+  console.log("value of i is currently: ", i);
+} while (i < 5)
