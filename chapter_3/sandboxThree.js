96401403c1425f0db877ccab370515993f995ebd
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-03 16:35:59 +0100

    Adding 3.22 and 3.23, if, else and else if statements.

--- chapter_3/sandboxThree.js
+++ chapter_3/sandboxThree.js
@@ -34,9 +34,58 @@
 // // 3.21 Do While loops
 
 
-let i = 10;
+// let i = 10;
 
- do {
-  i++;
-  console.log("value of i is currently: ", i);
-} while (i < 5)
+//  do {
+//   i++;
+//   console.log("value of i is currently: ", i);
+// } while (i < 5)
+
+// // 3.22 if statement
+
+// let age = 20
+// if(age < 100) {
+//   console.log("Hi there young one")
+// }
+
+// const ninjas = ['Johan', 'Charlie', 'Charlene', 'WolfDogg', 'OverLoard']
+
+// if(ninjas.length >= 5) {
+//   console.log('We officially have a ninja clan')
+// }
+
+// password = 'Grävlingen'
+
+// if (password.length > 4) {
+//   console.log('long enough')
+// }
+
+// if (password.length > 4) console.log('long enough')
+
+// // 3.22 Else and else if
+
+password = 'Grävlingen'
+
+if (password.length > 4) {
+  console.log('long enough')
+} else {
+  console.log('not long enough')
+}
+
+password = 'Grä'
+
+if (password.length > 4) {
+  console.log('long enough')
+} else {
+  console.log('not long enough')
+}
+
+password = 'hejad'
+
+if(password.length < 4) {
+  console.log('that password is so weak bruh')
+} else if(password.length < 8) {
+  console.log('still not enough bruh')
+} else {
+  console.log("wow bruh, you're a ninja")
+}
