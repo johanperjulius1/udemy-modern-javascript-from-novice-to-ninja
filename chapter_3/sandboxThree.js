e5fef5776b5a5bcc2f46f6905d00956e0abaa249
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-03 15:06:20 +0100

    Adding a seperate js file for chapter 3 and moving index.html to root folder. Finishing 3.19 - For loops

---
+++ chapter_3/sandboxThree.js
@@ -0,0 +1,13 @@
+// for (let i = 0; i < 5; i++) {
+//   console.log("in loop :", i);
+// }
+
+// console.log('The loop is now finished')
+
+const names = ["shaun", "mario", "Johnny"];
+
+for (let i = 0; i < names.length; i++) {
+  // console.log(names[i])
+  let html = `<div>${names[i]}</div>`;
+  console.log(html)
+}
