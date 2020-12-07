e8dc1341f230fbcd6a49df0c0c77cee278ccef42
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-07 15:26:29 +0100

    Adding chapter 4.30 Function Declarations and expressions.

---
+++ chapter_4/sandboxFour.js
@@ -0,0 +1,24 @@
+// // 4.30 Function Declaration & Expressions
+
+greet();
+
+// function declarations
+
+function greet() {
+  console.log("hello from declaration");
+}
+
+const johan = greeting = () => {
+  console.log("Hi from arrow function expression");
+};
+
+johan();
+
+
+ ludwig = () => {
+  console.log("Hi from arrow function declaration");
+};
+
+johan();
+ludwig();
+greeting();
