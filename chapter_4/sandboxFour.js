18e5ee806b4fa09d3bc7f64ab60ecc5bfaf0bc9b
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-08 16:37:07 +0100

    4.31 Parameters & Arguments.

--- chapter_4/sandboxFour.js
+++ chapter_4/sandboxFour.js
@@ -1,24 +1,32 @@
 // // 4.30 Function Declaration & Expressions
 
-greet();
+// greet();
 
-// function declarations
+// // function declarations
 
-function greet() {
-  console.log("hello from declaration");
-}
+// function greet() {
+//   console.log("hello from declaration");
+// }
 
-const johan = greeting = () => {
-  console.log("Hi from arrow function expression");
-};
+// const johan = greeting = () => {
+//   console.log("Hi from arrow function expression");
+// };
 
-johan();
+// johan();
 
 
- ludwig = () => {
-  console.log("Hi from arrow function declaration");
-};
+//  ludwig = () => {
+//   console.log("Hi from arrow function declaration");
+// };
 
-johan();
-ludwig();
-greeting();
+// johan();
+// ludwig();
+// greeting();
+
+// // 4.31 Arguments & Parameters
+
+// const speak = (name = 'brauser', time='morning') => {
+//   console.log(`good ${time} ${name}`)
+// }
+
+// speak('eric', 'daylight')
