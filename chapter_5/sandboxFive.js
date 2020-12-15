966ceab3d859dacc40446374de656f47b891a631
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-15 18:58:37 +0100

    5.40

--- chapter_5/sandboxFive.js
+++ chapter_5/sandboxFive.js
@@ -1,17 +1,21 @@
-// // 5.37, 5.38, 5.39 Creating an object literal & Adding Methods
-let rainman = {
-  realName: "Johnny",
-  age: 16,
-  email: 'will never say',
-  favouriteGame: "BlackJack of course",
-  biggestWins: [4200, 663],
-  login: () => {
-    console.log('rainman is at the table ready to play')
-  },
-  logBlogs: () => {
-    console.log(biggestWins)
-  }
-};
+// // // 5.37, 5.38, 5.39 5.40 Creating an object literal & Adding Methods & This Keyword
+// let rainman = {
+//   realName: "Johnny",
+//   age: 16,
+//   email: "will never say",
+//   favouriteGame: "BlackJack of course",
+//   biggestWins: [4200, 663],
+//   login: () => {
+//     console.log("rainman is at the table ready to play");
+//   },
+//   logWins: function () {
+//     // console.log(this.biggestWins[0]);
+//     console.log("rainmans biggest wins to date are:");
+//     this.biggestWins.forEach(biggestWin => {
+//       console.log(biggestWin)
+//     });
+//   },
+// };
 
-rainman.logBlogs()
-
+// rainman.logWins();
+// // console.log(this);
