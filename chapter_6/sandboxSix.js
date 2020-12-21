2a9c224eb41420f6839e7adecee0b0f8bfe10d47
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-21 23:21:25 +0100

    6.47

--- chapter_6/sandboxSix.js
+++ chapter_6/sandboxSix.js
@@ -1,20 +1,30 @@
-const para = document.querySelector("div.best");
-const para_2 = document.querySelector(
-  "body > div:nth-child(1) > p:nth-child(3)"
-);
-const allParas = document.querySelectorAll("p");
-console.log(para);
-console.log(para_2);
-console.log(`här kommer alla p på en och samma gång: ${allParas}`);
-console.log(allParas[1]);
-allParas.forEach((enparaitaget) => {
-  console.log(enparaitaget);
-});
+// const para = document.querySelector("div.best");
+// const para_2 = document.querySelector(
+//   "body > div:nth-child(1) > p:nth-child(3)"
+// );
+// const allParas = document.querySelectorAll("p");
+// console.log(para);
+// console.log(para_2);
+// console.log(`här kommer alla p på en och samma gång: ${allParas}`);
+// console.log(allParas[1]);
+// allParas.forEach((enparaitaget) => {
+//   console.log(enparaitaget);
+// });
 
-const divList = document.querySelectorAll("div");
-console.log(divList);
+// const divList = document.querySelectorAll("div");
+// console.log(divList);
 
-const classBestList = document.querySelectorAll(".best");
-console.log(classBestList);
+// const classBestList = document.querySelectorAll(".best");
+// console.log(classBestList);
 
- 
+// //6.47
+
+// const title = document.getElementById("page-headline");
+// console.log(title);
+
+// const divList2 = document.getElementsByClassName("best");
+// console.log(divList2);
+
+// const tags = document.getElementsByTagName("p");
+// console.log(tags);
+// console.log(tags[1]);
