d29dd9b95b1a5a8365a11c0de7ae61c49074b34b
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-12-28 23:45:59 +0100

    6.53 Event Basics.

--- chapter_6/sandboxSix.js
+++ chapter_6/sandboxSix.js
@@ -97,17 +97,33 @@
 
 // // 6.52 Parents, Children & Siblings
 
-const article = document.querySelector("article");
+// const article = document.querySelector("article");
 // console.log(article);
 // console.log(Array.from(article.children));
 
 // Array.from(article.children).forEach((child) => {
 //   child.classList.add("article-element");
 // });
-const title = document.querySelector("p");
-console.log(title.parentElement);
-console.log(title.parentElement.parentElement);
-console.log(title.nextElementSibling);
-console.log(title.previousElementSibling);
+// const title = document.querySelector("p");
+// console.log(title.parentElement);
+// console.log(title.parentElement.parentElement);
+// console.log(title.nextElementSibling);
+// console.log(title.previousElementSibling);
 
-console.log(title.nextElementSibling.parentElement.children);
+// console.log(title.nextElementSibling.parentElement.children);
+
+// const button = document.querySelector("button");
+
+// button.addEventListener('click', () =>{
+//   console.log('you clicked me!!')
+// })
+
+const items = document.querySelectorAll("li");
+items.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(item.textContent);
+    console.log(e.target.textContent);
+     e.target.classList.toggle('strikeThrough')
+    // e.target.style.textDecoration = 'line-through'
+  });
+});
