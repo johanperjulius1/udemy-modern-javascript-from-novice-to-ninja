341080a88a2a7b78f2733a8f51ecc5711cd9bbb5
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2021-01-14 00:38:10 +0100

    11.84 Adding the core clock functionality.

--- chapter_11/sandboxEleven.js
+++ chapter_11/sandboxEleven.js
@@ -45,17 +45,42 @@
 
 // // 11.83 Timestamps & Comparisons
 
-const before = new Date("July 10 1994 17:12:00");
-const now = new Date();
+// const before = new Date("July 10 1994 17:12:00");
+// const now = new Date();
 
-console.log(now.getTime(), before.getTime());
-const ageInMilliseconds = now.getTime() - before.getTime();
-const ageInSeconds = Math.round(ageInMilliseconds / 1000);
-const ageInMins = Math.round(ageInSeconds / 60);
-const ageInHours = Math.round(ageInMins / 60);
-const ageInDays = Math.round(ageInHours / 24);
-const ageInYears = Math.round(ageInDays / 365);
+// console.log(now.getTime(), before.getTime());
+// const ageInMilliseconds = now.getTime() - before.getTime();
+// const ageInSeconds = Math.round(ageInMilliseconds / 1000);
+// const ageInMins = Math.round(ageInSeconds / 60);
+// const ageInHours = Math.round(ageInMins / 60);
+// const ageInDays = Math.round(ageInHours / 24);
+// const ageInYears = Math.round(ageInDays / 365);
 
-const superstar = console.log("This SUPERSTAR was born ", ageInYears, " ago");
-const timestamp = 773853120000;
-console.log(new Date(timestamp));
+// const superstar = console.log("This SUPERSTAR was born ", ageInYears, " ago");
+// const timestamp = 773853120000;
+// console.log(new Date(timestamp));
+
+// // 11.84 Building & Digital clock
+
+// const now = new Date();
+// let clock = () => { document.querySelector(".clock").innerHTML= now;}
+// setInterval(clock(), 1000);
+
+const clock = document.querySelector(".clock");
+
+const tick = () => {
+  const now = new Date();
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const html = `
+    <span>${hours}</span> :
+    <span>${minutes}</span> :
+    <span>${seconds}</span>
+    `
+    clock.innerHTML = html
+};
+
+setInterval(tick, 1000)
