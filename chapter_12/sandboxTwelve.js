093d2893a650c5579c248a23f59e4cd966dc79df
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2021-08-18 16:49:25 +0200

    adding chapter 12.93. Nesting callbacks.

--- chapter_12/sandboxTwelve.js
+++ chapter_12/sandboxTwelve.js
@@ -1,31 +1,51 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
 
   const request = new XMLHttpRequest();
 
   request.addEventListener('readystatechange', () => {
     // console.log(request, request.readyState);
     if(request.readyState === 4 && request.status === 200) {
-      callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4 ){
       callback('could not fetch data', undefined);
     }
   })
 
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/1')
+  request.open('GET', resource)
   request.send();
 }
 
 console.log(1)
 
-getTodos((err, data)=> {
-  console.log('callback fired')
+getTodos('../chapter_12/todos/carro.json', (err, data)=> {
   if(err) {
     console.log(err)
     return;
   }
     console.log(data)
+    getTodos('../chapter_12/todos/pappa.json', (err, data)=> {
+      if(err) {
+        console.log(err)
+        return;
+      }
+        console.log(data)
+        getTodos('../chapter_12/todos/mamma.json', (err, data)=> {
+          if(err) {
+            console.log(err)
+            return;
+          }
+            console.log(data)
+            return;
+        });
+        return;
+      });
     return;
 });
+
+
+
+
 
 console.log(2)
 
